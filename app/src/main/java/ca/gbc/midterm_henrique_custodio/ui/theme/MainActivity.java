7534c16b874c75f50d9c5839e5b542e66be07c4d
hcustod <h.custodio.dev@gmail.com>
2025-10-30 22:16:09 -0400

    Add: Controller and MainActivity done

--- app/src/main/java/ca/gbc/midterm_henrique_custodio/ui/theme/MainActivity.java
+++ app/src/main/java/ca/gbc/midterm_henrique_custodio/ui/theme/MainActivity.java
@@ -1,27 +1,84 @@
 package ca.gbc.midterm_henrique_custodio.ui.theme;
 
-import java.util.ArrayList;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.view.*;
 
-public class MainActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import ca.gbc.midterm_henrique_custodio.R;
 
-    protected void onCreate() {
+import ca.gbc.midterm_henrique_custodio.ui.theme.cache.DataCache;
+import ca.gbc.midterm_henrique_custodio.ui.theme.controller.MainController;
+import ca.gbc.midterm_henrique_custodio.ui.theme.listeners.mainListener;
 
+public class MainActivity extends AppCompatActivity {
+    private EditText editNumber;
+    private Button btnGenerate;
+    private Button btnHistory;
+    private ListView listTable;
+
+    private ArrayAdapter<String> tableAdapter;
+    private MainController controller;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startViews();
+        startAdapter();
+        startController();
+        startListeners();
     }
 
     private void startViews() {
-
+        editNumber = findViewById(R.id.editNumber);
+        btnGenerate = findViewById(R.id.btnGenerate);
+        btnHistory = findViewById(R.id.btnHistory);
+        listTable = findViewById(R.id.listTable);
     }
 
     private void startAdapter() {
-
+        tableAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                DataCache.currentTable
+        );
+        listTable.setAdapter(tableAdapter);
     }
 
     private void startController() {
-
+        controller = new MainController(
+                this,
+                editNumber,
+                tableAdapter
+        );
     }
 
     private void startListeners() {
-
+        mainListener.bind(btnGenerate, btnHistory, listTable, controller);
     }
-    
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, 1, 0, "Clear All");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == 1) {
+            controller.onClearAllRequested();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 }
--- app/src/main/java/ca/gbc/midterm_henrique_custodio/ui/theme/controller/MainController.java
+++ app/src/main/java/ca/gbc/midterm_henrique_custodio/ui/theme/controller/MainController.java
@@ -1,9 +1,15 @@
 package ca.gbc.midterm_henrique_custodio.ui.theme.controller;
 
 import android.content.Context;
+import android.content.Intent;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.content.DialogInterface;
+
+import ca.gbc.midterm_henrique_custodio.ui.theme.cache.DataCache;
+import androidx.appcompat.app.AlertDialog;
+
 
 public class MainController {
 
@@ -37,18 +43,74 @@ public class MainController {
     }
 
     public void generateTable(int num) {
+        DataCache.currentTable.clear();
+        for (int i = 1; i <= 10; i++) {
+            int result = num * i;
+            String row = num + " x " + i + " = " + result;
+            DataCache.currentTable.add(row);
+        }
 
+        if (!DataCache.history.contains(num)) {
+            // Fix needed for int casting to string
+            DataCache.history.add(num);
+        }
     }
 
     public void onRowSelected(int pos) {
+        final String selectedRow = DataCache.currentTable.get(pos);
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Delete Row");
+        builder.setMessage("Delete this row?\n" + selectedRow);
+
+        builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                DataCache.currentTable.remove(pos);
+                tableAdapter.notifyDataSetChanged();
+
+                Toast.makeText(context,
+                        "Deleted: " + selectedRow,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", null);
+        builder.show();
     }
 
     public void onHistoryClicked() {
+        Intent intent = new Intent(context, SecondActivity.class);
+        context.startActivity(intent);
 
     }
 
     public void onClearAllRequested() {
 
+        if (DataCache.currentTable.isEmpty()) {
+            Toast.makeText(context, "Nothing to clear", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Clear All");
+        builder.setMessage("Delete ALL rows?");
+
+        builder.setPositiveButton("Clear", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                DataCache.currentTable.clear();
+                tableAdapter.notifyDataSetChanged();
+
+                Toast.makeText(context,
+                        "All rows cleared",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", null);
+        builder.show();
     }
 }
